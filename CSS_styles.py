3d0dc41d3af2126dd6537f54b56c9b00a4f4b7b9
Richard Libreros <34092193+caozrich@users.noreply.github.com>
2023-05-24 04:42:19 -0500

    Update CSS_styles.py

--- CSS_styles.py
+++ CSS_styles.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import base64
 
-def set_bg_hack(main_bg):
+def apply_styles():
 
         main_bg_ext = "png"
        
@@ -10,11 +10,11 @@ def set_bg_hack(main_bg):
             f"""
             <style>
             .stApp {{
-                background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+                background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open('background.png', "rb").read()).decode()});
                 background-size: cover
             }}
             </style>
             """,
             unsafe_allow_html=True
         )
-set_bg_hack('background.png')
+
