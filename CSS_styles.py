cbceae161bc68402be12c416dab3f6af7a304fd8
Richard Libreros <34092193+caozrich@users.noreply.github.com>
2023-05-24 03:56:09 -0500

    Add files via upload

---
+++ CSS_styles.py
@@ -0,0 +1,17 @@
+def set_bg_hack(main_bg):
+
+        main_bg_ext = "png"
+       
+        st.markdown(
+            f"""
+            <style>
+            .stApp {{
+                background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+                background-size: cover
+            }}
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+
+    set_bg_hack('background.png')
